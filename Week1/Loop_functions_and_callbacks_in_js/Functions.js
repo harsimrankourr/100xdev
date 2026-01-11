a955e01355c7d13e38c2534bf161d3b7f08fff33
harsimrankourr <harsimrankour48@gmail.com>
2026-01-11 15:04:26 +0530

    Week1 Notes

---
+++ Week1/Loop_functions_and_callbacks_in_js/Functions.js
@@ -0,0 +1,25 @@
+/*Functions
+Functions are the set of statements that perform a task and calculate value 
+It should take and input and return some output where there is some obvious relation between the input and the output 
+*/
+
+
+//Declaring a function.
+function findSum(n){
+    let ans = 0;
+    for (let i=1; i<n; i++){
+        ans = ans+1;
+    }
+    return ans;
+}
+//===================================================================
+
+function findSum(n){
+    let ans = 0;
+    for (let i=1; i<n; i++){
+        ans = ans+1;
+    }
+    return ans;
+}
+let ans = findSum(100)
+console.log(ans);
