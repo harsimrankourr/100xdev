07a948b6004e7500405944d618e4cf694f60cb75
harsimrankourr <harsimrankour48@gmail.com>
2026-01-13 14:59:19 +0530

    Http server2

---
+++ Week2/http_server2/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/route-handler', function(req, res)  {
+  res.JSON({
+    name: "harsimran",
+    age: 19
+  })
+})
+
+app.get('/', function(req, res)  {
+  res.send('Hello World!')
+})
+
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}`)
+})
