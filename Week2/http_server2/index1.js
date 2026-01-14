18fe0e127e88df6906474d799b7c90c62dc998f7
harsimrankourr <harsimrankour48@gmail.com>
2026-01-14 16:25:19 +0530

    Http server2

--- Week2/http_server2/index1.js
+++ Week2/http_server2/index1.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const bodyParser = require("body-parser");
 const app = express()
@@ -9,6 +10,41 @@ app.use(bodyParser.urlencoded());
 app.get('/', function(req, res)  {
     console.log(req.body);
   res.send('Hello World!')
+})
+
+
+app.listen(port, function()  {
+  console.log(`Example app listening on port ${port}`)
+})
+  */
+
+//to send a request from browser to here we use 
+//fetch
+
+//here port is 3000
+//if there is not any existing posrt
+//by default its port is 443
+
+const express = require('express')
+const bodyParser = require("body-parser");
+const app = express()
+const port = 3000
+
+//Middlewares
+app.use(bodyParser.json());
+
+app.get('/', function(req, res)  {
+    console.log(req.body);
+  res.send('Hello World!')
+})
+
+app.post('/backend-api/conversation', function(req, res){
+    const message = req.body.message;
+    console.log(message);
+    //machine learning thing
+    req.json({
+        output: "2+2 = 4"
+    })
 })
 
 app.listen(port, function()  {
