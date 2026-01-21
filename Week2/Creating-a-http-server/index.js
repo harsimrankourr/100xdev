20ac41aa4e662fe87cc01e6771e809bce6460225
harsimrankourr <harsimrankour48@gmail.com>
2026-01-21 21:09:39 +0530

    http server using express

--- Week2/Creating-a-http-server/index.js
+++ Week2/Creating-a-http-server/index.js
@@ -18,6 +18,7 @@ app.listen(3001);
 
 */
 
+/*
 const express = require("express")
 
 function calculateSum(n){
@@ -37,8 +38,30 @@ app.get("/", function(req, res){
 })
 
 app.listen(3000);
+*/
 
 //how to reach there
 //localhost:3000/?n=30
 //type this in browser
+
+const express = require("express")
+
+function calculateSum(n){
+    let ans = 0;
+    for(let i=1; i<=n; i++){
+        ans = ans + i;
+    }
+    return ans;
+}
+
+const app = express();
+
+app.get("/", function(req, res){
+    const n= req.query.n
+    const ans = calculateSum(n)
+    res.send("hi your ans is" + ans.toString());
+})
+
+app.listen(3000);
+
 
