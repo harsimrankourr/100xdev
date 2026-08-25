3552b8440194e400ba85bfb04a1f6287485fb44f
harsimrankourr <harsimrankour48@gmail.com>
2026-08-25 12:37:22 +0530

    Callbacks in js

---
+++ Week1/Callbacks.js
@@ -0,0 +1,23 @@
+function sum(num1, num2, fnToCall) {
+    let result = num1 + num2;
+    fnToCall(result);
+}
+
+function displayResult(data) {
+    console.log("Result of the sum is: " + data);
+}
+
+const ans = sum(1, 2, fnToCall);
+
+//====================================================
+
+function calculateArithmatic(a, b, arithmaticFinalFunction) {
+    const ans = arithmaticFinalFunction(a, b)
+    return ans;
+}
+
+function sum(a, b) {
+    return a + b;
+}
+const value = calculateArithmatic(1, 2, sum);
+console.log(value);
