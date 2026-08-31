18461110a6853f7f45abd95be507f198ed3928a6
harsimrankourr <harsimrankour48@gmail.com>
2026-08-31 18:32:09 +0530

    ZOD schema

---
+++ Week3/2.ZOD.js
@@ -0,0 +1,32 @@
+// How can we do batter input validation?
+
+// There are some input validation libraries
+// ZOD is one of the such libraries
+
+// ZOD is typescript-first schema validation with static type inference.
+
+const express = require("express");
+const z = require("zod");
+const app = express();
+
+const schema = ZodAny.array(ZodAny.number());
+
+app.use(express.json());
+
+app.post("/health-checkup", function (req, res) {
+    //kidney = [1,2]
+    const kidneys = req.body.kidneys;
+
+    const response = schema.safeParse(kidneys)
+    if (!response.success) {
+        res.status(411).json({
+            msg: "input is invalid"
+        })
+    } else {
+        res.send({
+            response
+        })
+    }
+});
+
+app.listen(3000);
