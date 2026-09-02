92e6a6abe42ee5ea842d1d4888eb9eeabde22b72
harsimrankourr <harsimrankour48@gmail.com>
2026-09-02 18:44:03 +0530

    Creating a database

--- Week3/6.Databases.js
+++ Week3/6.Databases.js
@@ -23,15 +23,48 @@ Using libraries!
 - Jsonwebtokens library lets us to create jets
 - Mongoose lets us connect to our database 
 */
+//-------------------------------------------------------------------------------
 
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb+srv://harsimrankour48_db_user:X5LWDocMJgfTCSn1@cluster0.n5a7qcq.mongodb.net/myDatabase");
+
+// const User = mongoose.model('user', { name: String, email: String, password: String });
+
+//   const user = new User({
+//     name: 'Harsimran kaur',
+//     email: 'harsimrankour48@gmail.com',
+//     password: '1234'
+// });
+// user.save();
+
+//--------------------------------------------------------------------------------
+
+const express = require("express")
 const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json())
+
 mongoose.connect("mongodb+srv://harsimrankour48_db_user:X5LWDocMJgfTCSn1@cluster0.n5a7qcq.mongodb.net/myDatabase");
 
 const User = mongoose.model('user', { name: String, email: String, password: String });
 
-const user = new User({
-    name: 'Harsimran kaur',
-    email: 'harsimrankour48@gmail.com',
-    password: '1234'
-});
-user.save();
+app.post("/signup", async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const existingUser = await User.findOne({ email: username });
+    if (existingUser) {
+        return res.status(400).send("Username already exists");
+    }
+    const user = new User({
+        name: name,
+        email: username,
+        password: password
+    });
+    user.save();
+    res.json({
+        "msg": "User created successfully"
+    })
+})
