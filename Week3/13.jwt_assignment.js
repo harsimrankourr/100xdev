c3b2c4486cae02dca3574683bed1a6654f37a965
harsimrankourr <harsimrankour48@gmail.com>
2026-09-03 18:53:47 +0530

    Jwt assignment

---
+++ Week3/13.jwt_assignment.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const jwtPassword = "secret";
+const zod = require("zod");
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+function signJwt(username, password) {
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+
+    if (!usernameResponse.success || !passwordResponse.success) {
+        return null;
+    }
+
+    const signature = jwt.sign({
+        username
+    }, jwtPassword);
+
+    return signature;
+}
+
+  const ans = signJwt("harsimrankour48@gmail.com", "dabhbu")
+    console.log(ans);
