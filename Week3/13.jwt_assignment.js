f00463e0e89e05b7ffea3acbd6573fe311675ccc
harsimrankourr <harsimrankour48@gmail.com>
2026-09-03 19:11:35 +0530

    Jwt assignment

--- Week3/13.jwt_assignment.js
+++ Week3/13.jwt_assignment.js
@@ -20,5 +20,30 @@ function signJwt(username, password) {
     return signature;
 }
 
-  const ans = signJwt("harsimrankour48@gmail.com", "dabhbu")
-    console.log(ans);
+const ans = signJwt("harsimrankour48@gmail.com", "dabhbu")
+console.log(ans);
+
+function decodeJwt(token) {
+    //true, false
+    const decoded = jwt.decode(token);
+    if (decoded) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(decodeJwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiaGFyc2ltcmFuIiwiYWNjb3VudE51bWJlciI6MTIxMjMyMywiaWF0IjoxNzg4NDM3MTU3fQ.Ieksceyn6NsKtSIO_Fv0K_YQLJdHG0eJrbveqN8rrhI"))
+
+function verifyJwt(token) {
+    let ans = true;
+    try {
+        const verified = jwt.verify(token, jwtPassword);
+    } catch (e) {
+        ans1 = false;
+    }
+    return ans1;
+}
+
+const ans1 = verifyJwt("hhfjhvreunajkeifndsm");
+console.log(ans);
