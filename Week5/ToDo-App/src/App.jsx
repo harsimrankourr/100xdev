132c301e6053596fab8643adb621428d24fa4a7c
harsimrankourr <harsimrankour48@gmail.com>
2026-09-14 18:34:23 +0530

    Updating ToDos using react

--- Week5/ToDo-App/src/App.jsx
+++ Week5/ToDo-App/src/App.jsx
@@ -15,6 +15,23 @@ function App() {
     completed: true
   }]);
 
+  function addTodo() {
+
+    // creating a new value
+    // react would know that it need to re-render
+    // it only knows that the todos have changed
+    // and update like this 
+
+    //[1,2]
+    //[...todos] => [1,2]
+
+    //[1,2]
+    //[...todos, 3] => [1,2,3]
+    setTodos([...todos, {
+      title: "new Todo",
+      description: "desc of new todo"
+    }])
+  }
 
   return (
     <div>
