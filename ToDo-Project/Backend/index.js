efda592759765dee1eb5ec11b2c3a034ab72e956
harsimrankourr <harsimrankour48@gmail.com>
2026-09-15 22:59:42 +0530

    Backend of ToDo project using zod

---
+++ ToDo-Project/Backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", function(req, res){
+const createPayload = req.body;
+const paredPayload = createTodo
+})
+
+app.get("/todos", function(req, res){
+
+})
+
+app.put("/completed", function(req,res){
+
+})
+
+
