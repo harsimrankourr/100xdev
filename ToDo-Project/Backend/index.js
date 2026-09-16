fd9c63a118005e2db60d58cd8034f92266f2fc18
harsimrankourr <harsimrankour48@gmail.com>
2026-09-16 19:07:23 +0530

    Data base of ToDo project

--- ToDo-Project/Backend/index.js
+++ ToDo-Project/Backend/index.js
@@ -1,19 +1,33 @@
 const express = require("express");
+const { createTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", function(req, res){
-const createPayload = req.body;
-const paredPayload = createTodo
+app.post("/todo", function (req, res) {
+    const createPayload = req.body;
+    const paredPayload = createTodo.safeParse(createPatload);
+    if (!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
 
-app.get("/todos", function(req, res){
+app.get("/todos", function (req, res) {
 
 })
 
-app.put("/completed", function(req,res){
-
+app.put("/completed", function (req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wring inputs",
+        })
+        return;
+    }
 })
 
 
