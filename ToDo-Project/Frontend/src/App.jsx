be28f503cbf529a864c368daec2ab8cd54883e78
harsimrankourr <harsimrankour48@gmail.com>
2026-09-23 18:38:36 +0530

    Jsx file in frontend

--- ToDo-Project/Frontend/src/App.jsx
+++ ToDo-Project/Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import{createTodo} from'./Components/CreateTodo'
 
 function App(){
 
   return (
     <div>
-       hi there
+       <CreateTodo></CreateTodo>
     </div>
   )
 }
